918aeb5d40ca06f036d88c43a112751938f9b1cb
Dang-Hyeonah <danghyeona0113@gmail.com>
2020-03-13 16:39:12 +0900

    code 9498

---
+++ 02_if/02_score.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int score;
+
+	cin >> score;
+
+	if (score >= 90 && score <= 100)
+		cout << "A" << endl;
+	else if (score >= 80)
+		cout << "B" << endl;
+	else if (score >= 70)
+		cout << "C" << endl;
+	else if (score >= 60)
+		cout << "D" << endl;
+	else
+		cout << "F" << endl;
+}
