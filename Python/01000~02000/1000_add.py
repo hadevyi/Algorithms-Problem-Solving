3741d4f644c532e93ebe18d39d16de420645bf9f
eona1301 <eona1301@gmail.com>
2021-02-14 23:35:17 +0900

    python beakjoon solution

---
+++ Python/01000~02000/1000_add.py
@@ -0,0 +1,3 @@
+indata = input("")
+indata = list(map(int, indata.split()))
+print(sum(indata))
---
+++ Python/01000~02000/1001_sub.py
@@ -0,0 +1,3 @@
+indata = input("")
+indata = list(map(int, indata.split()))
+print(indata[0] - indata[1])
