626a7ef3fb2f18313ab0430a7dcd73219787417f
eona1301 <dang0113@naver.com>
2022-06-15 10:45:47 +0900

    💻�boj : solved 2557 hello world

---
+++ Baekjoon/Theme01_IO/01_2557.java
@@ -0,0 +1,18 @@
+/**
+ * 1. 문제정보
+ *  - 제목 : Hello World
+ *  - 번호 : 2557번
+ *  - 링크 : https://www.acmicpc.net/problem/2557
+ * 2. 풀이핵심
+ *  - 입출력 기초
+ *  - 공백 및 요청 내용 확인 필수
+ * 3. 풀이후기
+ *  - 백준 풀이의 class name은 Main
+ *  - 출력의 공백, 특수기호(!)를 빼지말자
+ */
+
+public class main {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
