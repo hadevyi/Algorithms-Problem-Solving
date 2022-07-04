816f62a6aa5e8af97c4a8f333f38f0150525a439
eona1301 <dang0113@naver.com>
2022-07-04 19:59:29 +0900

    💻 boj : solved 8595

--- Baekjoon/Theme01_IO/11_8595.java
+++ Baekjoon/Theme01_IO/11_8595.java
@@ -10,22 +10,24 @@ import java.io.InputStreamReader;
  *  - 연속된 수 문자의 숫자 표기
  *  - 알파벳과 수 구별하기
  * 3. 풀이후기
- *  -
- *  -
+ *  - 무려 4번이나 틀렸다... 메인 로직은 처음부터 작성했는데, result의 자료형을 제대로 설정하지 못했다.
+ *  - 주어지는 단어의 길이가 5,000,000임을 제대로 확인하지 않았다. int의 최대 값은 2,147,483,647이 최대고 길이는 10이다.
+ *  - 첫 시작이 3이고 길이가 10인 수 하나만 와도 int의 값은 초과한다. 이제는 주어진 조건을 더 꼼꼼히 보는 습관을 가져야할 것같다.
  */
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int len = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        int result = 0, num = 0;
+        long result = 0;
+        int num = 0;
 
         for (int i = 0;i < len;i++) {
             char ch = str.charAt(i);
 
             if (ch >= '0' && ch <= '9') {
                 num = num * 10 + (ch - '0');
-            } else {
+            } else if (num != 0) {
                 result += num;
                 num = 0;
             }
