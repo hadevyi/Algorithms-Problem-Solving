d39ee0938e66af79e046920fabcd36e1d6639de2
eona1301 <eona1301@gmail.com>
2023-05-26 23:31:08 +0900

    🗃️ sql: solved 동물 수 구하기

---
+++ Programmers - SQL/Lv2/03_동물 수 구하기.sql
@@ -0,0 +1,11 @@
+/**
+ * 1. 문제정보
+ *  - 제목 : 동물 수 구하기
+ *  - 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/59406
+ *  2. 풀이핵심
+ *  - Table 전체의 Count
+ * 3. 풀이후기
+ *  - 간단한 구현
+ */
+SELECT COUNT(A.ANIMAL_ID)
+FROM ANIMAL_INS A;
