028ab5ef589d5e610a2ac814d06492353d5f31dd
eona1301 <eona1301@gmail.com>
2023-06-13 01:47:49 +0900

    🎨 style: add solved core and review

--- Programmers - SQL/Lv3/04_카테고리 별 도서 판매량 집계하기.sql
+++ Programmers - SQL/Lv3/04_카테고리 별 도서 판매량 집계하기.sql
@@ -3,9 +3,9 @@
  *  - 제목 : 카테고리 별 도서 판매량 집계하기
  *  - 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/144855
  *  2. 풀이핵심
- *  - 
+ *  - 조건에 맞게 year, month를 활용해 내용을 찾기
  * 3. 풀이후기
- *  - 
+ *  - 단계별로 진행하고 마지막에 group by로 진행하였음
  */
 SELECT B.CATEGORY, SUM(S.SALES) AS TOTAL_SALES
 FROM BOOK_SALES S
