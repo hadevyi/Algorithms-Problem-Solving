cb1934d75da5e440ced8690541096e74b39f80e5
eona1301 <eona1301@gmail.com>
2023-07-29 00:14:05 +0900

    💻 stl : implementing double linked list main

---
+++ Standard Template Library/Double Linked List/Main.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private final static int MAX_NODE = 10000;
+    private final static int ADD_HEAD = 1;
+    private final static int ADD_TAIL = 2;
+    private final static int ADD_NUM = 3;
+    private final static int FIND = 4;
+    private final static int REMOVE = 5;
+    private final static int PRINT = 6;
+    private final static int PRINT_REVERSE = 7;
+    private final static int END = 99;
+
+    private final static DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+
+    private static BufferedReader br;
+
+    public static void run() throws Exception {
+        int cmd, data, num, ret;
+        int[] output = new int[MAX_NODE];
+        String str;
+        StringTokenizer st;
+
+        while(true) {
+            str = br.readLine();
+            st = new StringTokenizer(str, " ");
+            cmd = Integer.parseInt(st.nextToken());
+
+            switch(cmd) {
+                case ADD_HEAD :
+                    data = Integer.parseInt(st.nextToken());
+                    doubleLinkedList.addNode2Head(data);
+                    break;
+
+                case ADD_TAIL :
+                    data = Integer.parseInt(st.nextToken());
+                    doubleLinkedList.addNode2Tail(data);
+                    break;
+
+                case ADD_NUM :
+                    data = Integer.parseInt(st.nextToken());
+                    num = Integer.parseInt(st.nextToken());
+                    doubleLinkedList.addNode2Num(data, num);
+                    break;
+
+                case FIND :
+                    data = Integer.parseInt(st.nextToken());
+                    num = doubleLinkedList.findNode(data);
+                    System.out.println(num);
+                    break;
+
+                case REMOVE :
+                    data = Integer.parseInt(st.nextToken());
+                    doubleLinkedList.removeNode(data);
+                    break;
+
+                case PRINT :
+                    ret = doubleLinkedList.getList(output);
+                    for(int i = 0; i < ret; i++) {
+                        System.out.print(output[i] + " ");
+                    }
+                    System.out.println();
+                    break;
+
+                case PRINT_REVERSE :
+                    ret = doubleLinkedList.getReversedList(output);
+                    for(int i = 0; i < ret; i++) {
+                        System.out.print(output[i] + " ");
+                    }
+                    System.out.println();
+                    break;
+
+                case END :
+                    return;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int TC;
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        TC = Integer.parseInt(str);
+
+        for (int tc = 1; tc <= TC; tc++) {
+            System.out.println("#" + tc);
+            doubleLinkedList.init();
+            run();
+            System.out.println();
+        }
+    }
+}
