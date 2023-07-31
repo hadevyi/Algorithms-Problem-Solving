19cf3f4c3a1876a7199e04da6418f2531afa9e46
eona1301 <eona1301@gmail.com>
2023-07-31 10:03:08 +0900

    💻 stl : implement main about breadth first search

---
+++ Standard Template Library/Breadth First Search/Main.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private final static BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
+
+    private static BufferedReader br;
+
+    private static int cmd_bfs() throws Exception {
+
+        int score = 100;
+        int N, M, x1, y1, x2, y2, dist, ans;
+        int[][] map = new int[10][10];
+        String str;
+        StringTokenizer st;
+
+        //System.setIn(new FileInputStream("src/input.txt"));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+        N = Integer.parseInt(str);
+
+        for(int i = 0; i < N; i++) {
+            str = br.readLine();
+            st = new StringTokenizer(str, " ");
+            for(int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        breadthFirstSearch.bfs_init(N, map);
+
+        str = br.readLine();
+        M = Integer.parseInt(str);
+
+        for(int i = 1; i <= M; i++) {
+            str = br.readLine();
+            st = new StringTokenizer(str, " ");
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+            ans = Integer.parseInt(st.nextToken());
+
+            dist = breadthFirstSearch.bfs(x1, y1, x2, y2);
+
+            if(dist != ans) {
+                score = 0;
+            }
+        }
+        return score;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("#total score : " + cmd_bfs());
+    }
+}
