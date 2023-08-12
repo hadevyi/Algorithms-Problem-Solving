6a9126d208fade6ff425297dd124bcd9ff4a7504
eona1301 <eona1301@gmail.com>
2023-08-12 15:22:44 +0900

    💻 stl : implement main in partial sort

---
+++ Standard Template Library/Parial Sort/Main.java
@@ -0,0 +1,71 @@
+import java.io.*;
+
+public class Main {
+
+    private final static int MAX_INPUT = 100000;
+    private final static int MAX_NUM = 30000;
+
+    private final static PartialSort partialSort = new PartialSort();
+
+    private static BufferedReader br;
+
+    private static int[] input = new int[MAX_INPUT];
+    private static long seed = 13410;
+
+    private static long pseudoRand() {
+        seed = (seed * 214013 + 2531011) & 0xffffffffL;
+        return (seed>>11) % MAX_NUM;
+    }
+
+    private static void makeInput(int inputLen) {
+        for(int i = 0; i < inputLen; i++) {
+            input[i] = (int)(pseudoRand());
+        }
+    }
+
+    private static int run() throws Exception {
+        int score = 100;
+        int N, userNum, uID = 0, ret, sum, ans;
+        int[] result = new int[10];
+        String str;
+
+        str = br.readLine();
+        N = Integer.parseInt(str);
+
+        for(int i = 0; i < N; i++) {
+            str = br.readLine();
+            userNum = Integer.parseInt(str);
+            makeInput(userNum);
+
+            for(int j = 0; j < userNum; j++) {
+                partialSort.addUser(uID++, input[j]);
+            }
+            ret = partialSort.getTop10(result);
+
+            sum = 0;
+            for(int j = 0; j < ret; j++) {
+                sum += result[j];
+            }
+
+            str = br.readLine();
+            ans = Integer.parseInt(str);
+            if(sum != ans) {
+                score = 0;
+            }
+        }
+        return score;
+    }
+    public static void main(String[] args) throws Exception {
+        int TC;
+        //System.setIn(new java.io.FileInputStream("src/dx2023task/basicheap/input.txt"));
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        TC = Integer.parseInt(str);
+
+        for (int tc = 1; tc <= TC; tc++) {
+            partialSort.init();
+            System.out.println("#" + tc + " " + run());
+        }
+    }
+}
