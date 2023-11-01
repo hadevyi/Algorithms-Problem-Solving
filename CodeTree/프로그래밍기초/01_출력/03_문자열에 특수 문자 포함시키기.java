2061f9a96b653b61d7d35d16cec3273bd99b6b89
eona1301 <eona1301@gmail.com>
2023-11-01 14:16:54 +0900

    💻 codetree: solved 문자열에 특수 문자 포함시키기

---
+++ CodeTree/프로그래밍기초/01_출력/03_문자열에 특수 문자 포함시키기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 문자열에 특수 문자 포함시키기
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/print-quote/introduction
+ *  2. 풀이핵심
+ *  - println
+ * 3. 풀이후기
+ *  - 간단한 문제풀이
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("He says \"It\'s a really simple sentence\".");
+    }
+}
