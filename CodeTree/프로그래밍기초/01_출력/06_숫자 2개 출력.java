677c2f716c5bef6acf6872f26803175ffdf95750
eona1301 <eona1301@gmail.com>
2023-11-03 14:18:36 +0900

    💻 codetree: solved 숫자 2개 출력

---
+++ CodeTree/프로그래밍기초/01_출력/06_숫자 2개 출력.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 숫자 2개 출력
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/print-two-numbers/description
+ *  2. 풀이핵심
+ *  - println
+ * 3. 풀이후기
+ *  - 간단한 문제풀이
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("3 5");
+    }
+}
