cc6482a8b1df3698fcaf977a7b1704714965d438
eona1301 <eona1301@gmail.com>
2023-11-05 14:19:37 +0900

    💻 codetree: solved 두줄 출력

---
+++ CodeTree/프로그래밍기초/01_출력/08_두줄 출력.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 두줄 출력
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/print-two-sentences-introduce/description
+ *  2. 풀이핵심
+ *  - println
+ * 3. 풀이후기
+ *  - 간단한 출력문제
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello students!");
+        System.out.println("Welcome to LeebrosCode!");
+    }
+}
