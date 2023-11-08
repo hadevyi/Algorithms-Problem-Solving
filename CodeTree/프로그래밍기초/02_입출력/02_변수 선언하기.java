88ee79b2a88b7c2f57071fbdd3aab8755870bd61
eona1301 <eona1301@gmail.com>
2023-11-08 14:28:43 +0900

    💻 codetree: solved 변수 선언하기

---
+++ CodeTree/프로그래밍기초/02_입출력/02_변수 선언하기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 변수 선언하기
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/declaring-variables/description
+ *  2. 풀이핵심
+ *  - 간단한 선언 및 출력
+ * 3. 풀이후기
+ *  - 쉬운 문제 풀이
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        char b = 'C';
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
