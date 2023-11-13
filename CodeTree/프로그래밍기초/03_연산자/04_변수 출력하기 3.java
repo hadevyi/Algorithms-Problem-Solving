5a655cfdc435ed759889cf3bd1fb5eb40c2e5837
eona1301 <eona1301@gmail.com>
2023-11-13 14:33:53 +0900

    💻 codetree: solved 변수 출력하기 3

---
+++ CodeTree/프로그래밍기초/03_연산자/04_변수 출력하기 3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 변수 출력하기 3
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/outputing-variables-3/description
+ *  2. 풀이핵심
+ *  - 간단한 변수 선언 및 출력
+ * 3. 풀이후기
+ *  - 쉬운 문제 풀이
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        char c = 'C';
+        System.out.println(a + "->" + b + "->" + c);
+    }
+}
