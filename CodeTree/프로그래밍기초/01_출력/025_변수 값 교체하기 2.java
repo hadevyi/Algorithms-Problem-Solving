752ce2bdd178f47c0d587dc47237612a41a1de12
eona1301 <eona1301@gmail.com>
2023-11-22 15:31:53 +0900

    💻 codetree: solved 변수 값 교체하기 2

---
+++ CodeTree/프로그래밍기초/01_출력/025_변수 값 교체하기 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 변수 값 교체하기 2
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/replacing-variable-values-2/submissions
+ *  2. 풀이핵심
+ *  - 간단한 문제 풀이
+ * 3. 풀이후기
+ *  - 쉬운 문제 풀이
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+        a = b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
