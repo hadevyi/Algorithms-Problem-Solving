68bfd6b6210336e8623b01f611dc79a541669778
eona1301 <eona1301@gmail.com>
2023-12-01 13:53:54 +0900

    💻 codetree: solved 정수 입력받아 계산

---
+++ CodeTree/프로그래밍기초/02_입출력/004_정수 입력받아 계산.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 정수 입력받아 계산
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/calculation-by-inputting-an-integer/description
+ *  2. 풀이핵심
+ *  - 입출력
+ * 3. 풀이후기
+ *  - 간단한 문제풀이
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine()) * 2 + 3;
+        System.out.println(a);
+    }
+}
