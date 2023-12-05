1333c083bff47b95d3bc58ab8d895dee83563dd0
eona1301 <eona1301@gmail.com>
2023-12-05 09:51:00 +0900

    💻 codetree: solved 문자열 순서 바꾸기

---
+++ CodeTree/프로그래밍기초/02_입출력/020_문자열 순서 바꾸기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 문자열 순서 바꾸기
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/change-order-of-strings/description
+ *  2. 풀이핵심
+ *  - 입력에 대한 처리
+ * 3. 풀이후기
+ *  - 간단한 문제 풀이
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String t = br.readLine();
+        System.out.println(t);
+        System.out.println(s);
+    }
+}
