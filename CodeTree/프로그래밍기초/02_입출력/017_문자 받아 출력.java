ff17e57c580132ea1d92767f3666299498759a01
eona1301 <eona1301@gmail.com>
2023-12-05 09:49:23 +0900

    💻 codetree: solved 문자 받아 출력

---
+++ CodeTree/프로그래밍기초/02_입출력/017_문자 받아 출력.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 문자 받아 출력
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/enter-char-and-print/description
+ *  2. 풀이핵심
+ *  - 입력 출력
+ * 3. 풀이후기
+ *  - 간단한 입력과 출력
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str);
+    }
+}
