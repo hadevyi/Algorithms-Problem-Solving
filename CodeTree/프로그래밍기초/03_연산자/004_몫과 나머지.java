07a27501f7b18ac07f1c0b80cd6ec8980ef206c4
eona1301 <eona1301@gmail.com>
2023-12-07 00:49:21 +0900

    💻 codetree: solved 몫과 나머지

---
+++ CodeTree/프로그래밍기초/03_연산자/004_몫과 나머지.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 몫과 나머지
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/quotient-remainder/description
+ * 2. 풀이핵심
+ *  - 연산자 활용
+ * 3. 풀이후기
+ *  - 간단한 문제 풀이
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println((a / b) + "..." + (a % b));
+    }
+}
