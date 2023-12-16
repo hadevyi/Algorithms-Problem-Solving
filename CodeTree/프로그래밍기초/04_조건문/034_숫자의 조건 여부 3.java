697a4027a551ca03ddd43dcb5f91edc149ffce79
eona1301 <eona1301@gmail.com>
2023-12-16 21:06:13 +0900

    💻 codetree: solved 숫자의 조건 여부 3

---
+++ CodeTree/프로그래밍기초/04_조건문/034_숫자의 조건 여부 3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 숫자의 조건 여부 3
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/number's-condition-3/description
+ * 2. 풀이핵심
+ *  - 복합조건 or로 연결하기
+ * 3. 풀이후기
+ *  - 간단한 문제 풀이
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.println(a % 13 == 0 || a % 19 == 0 ? "True" : "False");
+    }
+}
