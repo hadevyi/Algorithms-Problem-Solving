8a39118e77a022fb601bed916719f33a5f5c5b29
eona1301 <eona1301@gmail.com>
2024-01-05 22:27:23 +0900

    💻 codetree: solved 짝수만 출력

---
+++ CodeTree/프로그래밍기초/05_단순반복문/019_짝수만 출력.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 짝수만 출력
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/output-only-even/description
+ * 2. 풀이핵심
+ *  - 증가 반복문
+ * 3. 풀이후기
+ *  - 간단만 문제 풀이
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        StringBuffer sb = new StringBuffer();
+        for (int num = a;num <= b;num++) {
+            if (num % 2 != 0) continue;
+            sb.append(num).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
