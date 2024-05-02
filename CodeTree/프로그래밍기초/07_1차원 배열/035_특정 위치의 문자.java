441fe9a665163e49626d895f05df7badea0bebec
eona1301 <eona1301@gmail.com>
2024-05-02 22:36:31 +0900

    💻 codetree: solved 특정 위치의 문자

---
+++ CodeTree/프로그래밍기초/07_1차원 배열/035_특정 위치의 문자.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 특정 위치의 문자
+ *  - 링크 : 
+ * 2. 풀이핵심
+ *  - 탐색
+ * 3. 풀이후기
+ *  - 간단한 문제 풀이
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
