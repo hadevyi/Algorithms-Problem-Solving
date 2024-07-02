52776630ef56ea05d3880af95d3d426d99ae4300
eona1301 <eona1301@gmail.com>
2024-07-02 21:55:51 +0900

    💻 codetree: solved 문자열 추가하기

---
+++ CodeTree/프로그래밍기초/08_2차원 배열/034_문자열 추가하기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 문자열 추가하기
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/add-spring/description
+ * 2. 풀이핵심
+ *  - 문자열 concat
+ * 3. 풀이후기
+ *  - 간단한 문제 풀이
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.printf("%sHello", br.readLine());
+    }
+}
