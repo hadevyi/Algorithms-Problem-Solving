6c6e6bcc479f9b9ed6865aff5ac1229c3ce46455
eona1301 <eona1301@gmail.com>
2024-07-04 22:23:28 +0900

    💻 codetree: solved 문자열 나누기

--- CodeTree/프로그래밍기초/08_2차원 배열/036_문자열 나누기.java
+++ CodeTree/프로그래밍기초/08_2차원 배열/036_문자열 나누기.java
@@ -2,15 +2,26 @@ import java.io.*;
 import java.util.*;
 /**
  * 1. 문제정보
- *  - 제목 : 
- *  - 링크 : 
+ *  - 제목 : 문자열 나누기
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/divide-string/description
  * 2. 풀이핵심
  *  - 문자열 concat
  * 3. 풀이후기
  *  - 간단한 문제 풀이
  */
 public class Main {
+    public static int N;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        StringBuffer sb = new StringBuffer();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int n = 0;n < N;n++) sb.append(st.nextToken());
+        String str = sb.toString();
+
+        sb = new StringBuffer();
+        for (int n = 0;n < str.length();n += 5) sb.append(str.substring(n, Math.min(n + 5, str.length()))).append("\n");
+        System.out.println(sb.toString());
     }
 }
