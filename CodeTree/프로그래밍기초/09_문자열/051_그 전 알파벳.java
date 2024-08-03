afe2bd1964250a4a0c1cddfa94ca1dc18924d4f6
eona1301 <eona1301@gmail.com>
2024-08-03 23:49:57 +0900

    💻 codetree: solved 그 전 알파벳

---
+++ CodeTree/프로그래밍기초/09_문자열/051_그 전 알파벳.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 1. 문제정보
+ *  - 제목 : 그 전 알파벳
+ *  - 링크 : https://www.codetree.ai/missions/4/problems/before-the-alphabet/description
+ * 2. 풀이핵심
+ *  - 아스키 코드
+ * 3. 풀이후기
+ *  - 간단한 문제 풀이
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char ch = br.readLine().charAt(0);
+        System.out.println(ch == 'a' ? 'z' : (char) (ch - 1));
+    }
+}
