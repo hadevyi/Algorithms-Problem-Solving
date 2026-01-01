3e0b4ffe50b254a46fa641825d36cd17330da312
hadevyi <hadevyi@gmail.com>
2026-01-01 23:44:07 +0900

    🔥 init : 신규 알고리즘을 위한 초기화

--- SWEA/Professional/12303. 기초 Single Linked List 연습/Node.java
+++
@@ -1,9 +0,0 @@
-public class Node {
-    public int data;
-    public Node next;
-
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
--- SWEA/Professional/12318. 기초 BFS 연습/Node.java
+++
@@ -1,8 +0,0 @@
-public class Node {
-    int i, j;
-
-    public Node(int i, int j) {
-        this.i = i;
-        this.j = j;
-    }
-}
